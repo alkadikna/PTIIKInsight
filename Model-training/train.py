da3a51b6f6d1c36ad96195401e8742d92ab45994
Muhammad Rajendra Alkautsar Dikna <115086422+alkadikna@users.noreply.github.com>
2025-02-25 23:17:47 +0700

    model_v1

--- Model-training/train.py
+++ Model-training/train.py
@@ -1,1 +1,23 @@
-# train code here
+import pandas as pd
+from bertopic import BERTopic
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
+from tqdm import tqdm
+
+# Load data
+df = pd.read_csv('../data/cleaned_data.csv')
+texts = df['Judul'].tolist()
+
+# Create a CountVectorizer with Indonesian stop words
+vectorizer_model = CountVectorizer()
+
+# Model a.k.a BERTopic
+topic_model = BERTopic(vectorizer_model=vectorizer_model, language="multilingual")  
+topics, probabilities = topic_model.fit_transform(tqdm(texts, desc="Fitting BERTopic model"))
+
+# Save the model
+with open("bertopic_model_multi.pkl", "wb") as file:
+    pickle.dump(topic_model, file)
+df['topics'] = topics
+df['probabilities'] = probabilities
+df.to_csv('topic_modeling_results_multi.csv', index=False)
