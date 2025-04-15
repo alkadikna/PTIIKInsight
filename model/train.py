7392dd9a28ad76c698a908b801cb663ec174019a
Muhammad Rajendra Alkautsar Dikna <115086422+alkadikna@users.noreply.github.com>
2025-04-15 19:47:10 +0700

    train paraphrase-multilingual-MiniLM-L12-v2 and eval using

--- model/train.py
+++ model/train.py
@@ -9,16 +9,16 @@ from tqdm import tqdm
 df = pd.read_csv('../data/cleaned_data.csv')
 texts = df['Judul'].tolist()
 
-# Load AllenAI-SPECTER dengan CUDA
-embedding_model = SentenceTransformer("allenai-specter", device="cuda")
+# Load paraphrase-multilingual-MiniLM-L12-v2 dengan CUDA
+embedding_model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2", device="cuda")
 
 # Buat BERTopic dengan model embedding
 topic_model = BERTopic(embedding_model=embedding_model, language="multilingual")  
 topics, probabilities = topic_model.fit_transform(tqdm(texts, desc="Fitting BERTopic model"))
 
 # Save the model
-with open("bertopic_model_multi.pkl", "wb") as file:
+with open("bertopic_model.pkl", "wb") as file:
     pickle.dump(topic_model, file)
 df['topics'] = topics
 df['probabilities'] = probabilities
-df.to_csv('topic_modeling_results_multi_allenai.csv', index=False)
+df.to_csv('topic_modeling_results.csv', index=False)
